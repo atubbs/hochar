653c30486505403ef082969c5681e0662eda164c
Aaron N. Tubbs <atubbs@server.fake>
2015-02-03 01:37:52 +0000

    Branding is important, right?

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 var Datastore = require('nedb'), fs = require('fs');
 // TODO: this is hardcoded; may want it either some generic or something configurable
-var db = new Datastore({ filename: '/home/atubbs/code/CocktailEngine/data/tried_and_true.json', autoload: true });
+var db = new Datastore({ filename: '/home/atubbs/code/hochar/data/tried_and_true.json', autoload: true });
 module.exports = {
   handle : db
 };
